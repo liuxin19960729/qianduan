a551ae37c134686e50421de5ff7cc769a589044b
liuixn <963875368@qq.com>
2023-02-09 17:41:38 +0800

    微任务和宏任务学习

---
+++ js/基础/js宏任务和微任务.js
@@ -0,0 +1,20 @@
+
+// let end=0;
+
+// function callback(time){
+//     console.log(time-end)
+//     requestAnimationFrame(callback)
+//     end=time;
+//     let promsie=new Promise((res,rej)=>{
+//         res()
+//     });
+
+//     promsie.then(v=>{
+//         console.log("promise")
+//     })
+//     setTimeout(()=>{
+//         console.log("宏任务")
+//     });
+// }
+
+// requestAnimationFrame(callback)
